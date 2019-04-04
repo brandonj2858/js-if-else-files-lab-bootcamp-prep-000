ebe17ffb5c7bbdbb905fd576d9ff678d2db0c313
Brandon James <brandonj2858@gmail.com>
2019-04-04 19:45:39 +0000

    Automatically backed up by Learn

--- tests.js
+++ tests.js
@@ -5,5 +5,10 @@ console.log(index.name)
 if (name === Susan) {
   console.log("The name is correct")
 } else {
-  
+  console.log("Expected: Susan, Received: Joe")
+}
+
+if (height === 70) {
+  console.log("The name is correct")} else{
+  console.log("Expected: 70, Received: 74")
 }
